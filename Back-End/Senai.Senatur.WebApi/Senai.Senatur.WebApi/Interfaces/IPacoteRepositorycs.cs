b516501ba37c40ad8468baa64b5b3bebbf9c4835
Nícolas Silva Freitas <45300889873@info.local>
2020-03-10 10:00:19 -0300

    interfa v gt4

---
+++ Back-End/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Interfaces/IPacoteRepositorycs.cs
@@ -0,0 +1,20 @@
+﻿using Senai.Senatur.WebApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Senatur.WebApi.Interfaces
+{
+    interface IPacoteRepositorycs
+    {
+        List<Pacote> Listar();
+
+        void Cadastrar(Pacote pacote);
+
+
+        void Alterar(Pacote pacote);
+
+        Pacote BuscarPacotePorId(int id);
+    }
+}
