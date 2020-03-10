6f227ef383053d65069f1077e1ae63b8ce96aa36
Ana Laura Feltrim <46962757875@info.local>
2020-03-10 09:38:11 -0300

    com o andaime

---
+++ Back-End/Senai.Senatur.WebApi/Senai.Senatur.WebApi/Contexts/SenaturContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.Senatur.WebApi.Domains
+{
+    public partial class SenaturContext : DbContext
+    {
+        public SenaturContext()
+        {
+        }
+
+        public SenaturContext(DbContextOptions<SenaturContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pacote> Pacote { get; set; }
+        public virtual DbSet<TipoUsuario> TipoUsuario { get; set; }
+        public virtual DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source=DEV4\\SQLEXPRESS; Initial Catalog=Senatur_Manha; user Id=sa; pwd=sa@132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pacote>(entity =>
+            {
+                entity.HasKey(e => e.IdPacote);
+
+                entity.Property(e => e.DataIda).HasColumnType("date");
+
+                entity.Property(e => e.DataVolta).HasColumnType("date");
+
+                entity.Property(e => e.Descricao)
+                    .HasMaxLength(5000)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeCidade)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomePacote)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Valor).HasColumnType("decimal(18, 0)");
+            });
+
+            modelBuilder.Entity<TipoUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTipoUsuario);
+
+                entity.Property(e => e.Titulo)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(200)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .HasMaxLength(16)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTipoUsuarioNavigation)
+                    .WithMany(p => p.Usuario)
+                    .HasForeignKey(d => d.IdTipoUsuario)
+                    .HasConstraintName("FK__Usuario__IdTipoU__398D8EEE");
+            });
+        }
+    }
+}
